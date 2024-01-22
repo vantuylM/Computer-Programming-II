0c95a5ac6ac03bc4787f5481be04d2517a55dd1d
vantuylM <148888984+vantuylM@users.noreply.github.com>
2024-01-22 20:35:31 +0000

    1/22/2024

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
 print("Hello, World!")
+
+# This is a single-line commet
+
+""" Data Types:
+int - Integer (whole number)
+float - Floathing-point Number ( number w/ a decimal)
+str - String ( any text, in quotes)
+chr - Character ( single keyboard character, in single-quotes)
+bool- Boolean (either True or False only)
+"""
+
+""" Arithmetic Operators:
++ Add
+- Subtract
+* Multiply
+/ Divide
+// Floor Divide (Divide and round-down to nearest int)
+% Mod (Modulus/Remainder from Int division)
+** Pow
+"""
+
+""" Assignment Operators:
+= Valuse assignment (x = y)
++= Add to (x += y is shorthand for x = x + y)
+-= Subtract from (x -= y is shorthand for x = x - y)
+*/
+/=
+//=
+%=
+**=
+"""
---
+++ prog54b.py
@@ -0,0 +1,7 @@
+#Sum and Average of 4 three digit numbers
+Num1 = int(input("Enter Three Digit Number: "))
+Num2 = int(input("Enter Three Digit Number: "))
+Num3 = int(input("Enter Three Digit Number: "))
+Num4 = int(input("Enter Three Digit Number: "))
+Sum = Num1 + Num2 + Num3 + Num4
+print("Sum =", Sum)
