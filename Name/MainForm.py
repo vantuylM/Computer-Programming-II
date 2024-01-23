082d5109fdc450e1bb27b2a8ee701e7d9f09f130
vantuylM <vantuyl.m@stu.janesville.k12.wi.us>
2024-01-23 14:31:21 -0600

    1/23/2024
    1/23/2024

---
+++ Name/MainForm.py
@@ -0,0 +1,83 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self._button1 = System.Windows.Forms.Button()
+		self._label1 = System.Windows.Forms.Label()
+		self._button2 = System.Windows.Forms.Button()
+		self._button3 = System.Windows.Forms.Button()
+		self.SuspendLayout()
+		# 
+		# button1
+		# 
+		self._button1.BackColor = System.Drawing.Color.DarkGreen
+		self._button1.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button1.Location = System.Drawing.Point(13, 388)
+		self._button1.Name = "button1"
+		self._button1.Size = System.Drawing.Size(202, 169)
+		self._button1.TabIndex = 0
+		self._button1.Text = "Show"
+		self._button1.UseVisualStyleBackColor = False
+		self._button1.Click += self.Button1Click
+		# 
+		# label1
+		# 
+		self._label1.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._label1.Location = System.Drawing.Point(109, 140)
+		self._label1.Name = "label1"
+		self._label1.Size = System.Drawing.Size(430, 245)
+		self._label1.TabIndex = 1
+		self._label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		# 
+		# button2
+		# 
+		self._button2.BackColor = System.Drawing.Color.DarkGreen
+		self._button2.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button2.Location = System.Drawing.Point(248, 388)
+		self._button2.Name = "button2"
+		self._button2.Size = System.Drawing.Size(202, 169)
+		self._button2.TabIndex = 2
+		self._button2.Text = "Clear"
+		self._button2.UseVisualStyleBackColor = False
+		self._button2.Click += self.Button2Click
+		# 
+		# button3
+		# 
+		self._button3.BackColor = System.Drawing.Color.DarkGreen
+		self._button3.Font = System.Drawing.Font("Microsoft Sans Serif", 21.75, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
+		self._button3.Location = System.Drawing.Point(478, 388)
+		self._button3.Name = "button3"
+		self._button3.Size = System.Drawing.Size(202, 169)
+		self._button3.TabIndex = 3
+		self._button3.Text = "Exit"
+		self._button3.UseVisualStyleBackColor = False
+		self._button3.Click += self.Button3Click
+		# 
+		# MainForm
+		# 
+		self.BackColor = System.Drawing.Color.Green
+		self.ClientSize = System.Drawing.Size(692, 569)
+		self.Controls.Add(self._button3)
+		self.Controls.Add(self._button2)
+		self.Controls.Add(self._label1)
+		self.Controls.Add(self._button1)
+		self.Name = "MainForm"
+		self.Text = "MainForm"
+		self.ResumeLayout(False)
+
+
+	def Button1Click(self, sender, e):
+		self._label1.Text = "Michael Van Tuyl"
+
+	def Button2Click(self, sender, e):
+		self._label1.Text = ""
+
+	def Button3Click(self, sender, e):
+		Application.Exit()
