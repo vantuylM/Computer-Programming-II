2fd5747ef7bbb4ffcfe0bc2d9af22853948cbc73
vantuylM <148888984+vantuylM@users.noreply.github.com>
2024-02-14 20:22:35 +0000

    2/14/2024 update

--- encryptchat.py
+++ encryptchat.py
@@ -1,36 +1,36 @@
-import rsa # pip install rsa
+import rsa  # pip install rsa
 import socket
 import threading
 
-public_key, private_key = rsa.newkeys(1024) # 1024 bits
+public_key, private_key = rsa.newkeys(1024)  # 1024 bits
 DEFAULT_IP_PORT = ("127.0.0.1", 9999)
-choice = input("Do you want to be server or client? (s/c): ")
+choice = input("Do you want to be the server or client? (s/c): ")
 
 if choice == "s":
-  server = socket.socket(socket.AF_INET,   socket.SOCK_STREAM)
-  server.bind(DEFAULT_IP_PORT) # open tcp connection
+  server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  server.bind(DEFAULT_IP_PORT)  # Open TCP connection
   server.listen()
-  print("waiting for connection...")
+  print("Waiting for connection...")
   client, addr = server.accept()
   print("Connected to", addr)
   client.send(public_key.save_pkcs1())
-  public_partner =                     rsa.PublicKey.load_pkcs1(client.recv(1024))
-  print("Use 'ctrl+c' to disconnect.")
+  public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
+  print("Use 'Ctrl+C' to disconnect.")
 elif choice == "c":
   print("Connecting to server...", end="")
   client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   client.connect(DEFAULT_IP_PORT)
-  print("success! Connected to", client.getpeername())
+  print("success! Connected to " + str(client.getpeername()))
   public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
   client.send(public_key.save_pkcs1())
-  print("use 'ctrl+c' to disconnect.")
+  print("Use 'Ctrl+C' to disconnect.")
 else: exit()
 
 def sendMsg(c):
   while True:
     try:
       msg = input()
-      print('\033[1A' + '\033[K', end='')
+      print('\033[1A' + '\033[K', end='')  # Delete input line
       c.send(rsa.encrypt(msg.encode(), public_partner))
       print("\033[91mYou: \033[0m" + msg)
     except: exit()
@@ -41,10 +41,60 @@ def recvMsg(c):
       msg = rsa.decrypt(c.recv(1024), private_key)
       print("\033[94mPartner: \033[0m" + msg.decode())
     except:
-      input("Partner has disconnected. Press enter to exit.")
+      print("Partner has disconnected. Press enter to exit.")
       exit()
 
 try:
   send_thread = threading.Thread(target=sendMsg, args=(client,)).start()
-  recv_thread = thread.Thread(target=recvMsg, args=(client,)).start()
+  recv_thread = threading.Thread(target=recvMsg, args=(client,)).start()
 except: pass
+#import rsa # pip install rsa
+#import socket
+#import threading
+
+#public_key, private_key = rsa.newkeys(1024) # 1024 bits
+#DEFAULT_IP_PORT = ("127.0.0.1", 9999)
+#choice = input("Do you want to be server or client? (s/c): ")
+
+#if choice == "s":
+ # server = socket.socket(socket.AF_INET,   socket.SOCK_STREAM)
+ # server.bind(DEFAULT_IP_PORT) # open tcp connection
+ # server.listen()
+#  print("waiting for connection...")
+ # client, addr = server.accept()
+ # print("Connected to", addr)
+ # client.send(public_key.save_pkcs1())
+ # public_partner =                     rsa.PublicKey.load_pkcs1(client.recv(1024))
+#  print("Use 'ctrl+c' to disconnect.")
+#elif choice == "c":
+  #print("Connecting to server...", end="")
+ # client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#  client.connect(DEFAULT_IP_PORT)
+#  print("success! Connected to", client.getpeername())
+ # public_partner = rsa.PublicKey.load_pkcs1(client.recv(1024))
+ # client.send(public_key.save_pkcs1())
+  #print("use 'ctrl+c' to disconnect.")
+#else: exit()
+
+#def sendMsg(c):
+ # while True:
+  #  try:
+   #   msg = input()
+   #   print('\033[1A' + '\033[K', end='')
+   #   c.send(rsa.encrypt(msg.encode(), public_partner))
+   #   print("\033[91mYou: \033[0m" + msg)
+   # except: exit()
+
+#def recvMsg(c):
+  #while True:
+   # try:
+   #   msg = rsa.decrypt(c.recv(1024), private_key)
+   #   print("\033[94mPartner: \033[0m" + msg.decode())
+   # except:
+      #input("Partner has disconnected. Press enter to exit.")
+     # exit()
+
+#try:
+ # send_thread = threading.Thread(target=sendMsg, args=(client,)).start()
+ # recv_thread = thread.Thread(target=recvMsg, args=(client,)).start()
+#except: pass
