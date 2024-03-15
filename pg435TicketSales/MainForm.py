9ed10e98fd9f974ebd4a8707d310c92808f030e9
vantuylM <vantuyl.m@stu.janesville.k12.wi.us>
2024-03-15 14:35:22 -0500

    Update 3/15/2024

---
+++ pg435TicketSales/MainForm.py
@@ -0,0 +1,20 @@
+﻿import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class MainForm(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self.SuspendLayout()
+		# 
+		# MainForm
+		# 
+		self.ClientSize = System.Drawing.Size(806, 498)
+		self.Name = "MainForm"
+		self.Text = "pg435TicketSales"
+		self.ResumeLayout(False)
+
---
+++ pg435TicketSales/StudentForm1.py
@@ -0,0 +1,21 @@
+﻿
+import System.Drawing
+import System.Windows.Forms
+
+from System.Drawing import *
+from System.Windows.Forms import *
+
+class StudentForm1(Form):
+	def __init__(self):
+		self.InitializeComponent()
+	
+	def InitializeComponent(self):
+		self.SuspendLayout()
+		# 
+		# StudentForm1
+		# 
+		self.ClientSize = System.Drawing.Size(454, 330)
+		self.Name = "StudentForm1"
+		self.Text = "StudentForm1"
+		self.ResumeLayout(False)
+
