f4857834b4ef4a366c5acc7915d153936d596d7a
vantuylM <148888984+vantuylM@users.noreply.github.com>
2024-03-15 19:09:48 +0000

    Update 3/15/2024

--- Cl213f.py
+++ Cl213f.py
@@ -5,14 +5,15 @@ class Cl213f:
 
   def calc(self):
     if self.kwh <= 2000:
+      if self.kwh <= 8000:
+        if self.kwh <= 10000:
+          self.cost = 0.04
+      else:
+        self.cost = 0.05
+    else:
       self.cost = 0.07
-    if self.kwh <= 8000:
-      self.cost = 0.05
-    if self.kwh <= 10000:
-      self.cost = 0.04
-    if self.kwh > 10000:
-      self.cost = 0.04
     self.cost = self.kwh * self.cost
+    self.cost = round(self.cost, 2)
 
   def __str__(self):
     return f"The cost of {self.kwh} is {self.cost}"
