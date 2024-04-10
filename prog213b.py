1b282e11b2e1182013e00d475a8273529a9a99f6
vantuylM <148888984+vantuylM@users.noreply.github.com>
2024-04-10 19:00:30 +0000

    update

--- prog213b.py
+++ prog213b.py
@@ -2,23 +2,14 @@ with open("Langdat/prog213b.txt", 'r') as f:
   q = int(input("Enter quantity: "))
   total = 0.0
   price = 0.0
-  rline = f.readline()
-  if q <= 99:
-    price = 5.95
+  if q <= 99: price = 5.95
     
-  elif 99 < q <= 199:
-    price = 5.75
-    
-  elif 199 < q <= 299:
-    price = 5.40
-    
-  elif 299 < q:
-    price = 5.15
-
+  elif 99 < q <= 199: price = 5.75
+  elif 199 < q <= 299: price = 5.40    
+  elif 299 < q: price = 5.15
+  
   total = price * q
 
-  for line in f:
-    q = rline
 
   print("Price: " , price)
   print(f"Total: ", total)
