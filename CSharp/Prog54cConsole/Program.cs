d45aad9e4b458ba26c522e32544df96a777d822a
vantuylM <vantuyl.m@stu.janesville.k12.wi.us>
2024-04-11 14:17:50 -0500

    Update

---
+++ CSharp/Prog54cConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prog54cConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Radius: ");
+            double radius = double.Parse(Console.ReadLine);
+            double area = 3.141 * (radius * radius);
+        }
+    }
+}
