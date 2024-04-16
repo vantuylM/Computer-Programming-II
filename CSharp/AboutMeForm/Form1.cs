da0c936cc1ea57165409379d7044cb02d3ba7108
vantuylM <vantuyl.m@stu.janesville.k12.wi.us>
2024-04-16 14:35:16 -0500

    Update 4/16/2024

---
+++ CSharp/AboutMeForm/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AboutMeForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "My name is Michael Van Tuyl my favorate club is orchestra and my favorate quote is That's one small step for a man, a giant leap for mankind.	Neil Armstrong";
+        }
+    }
+}
