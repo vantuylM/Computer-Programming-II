84de0b7715d74d8284235dcc0cc286a39f7fb165
vantuylM <148888984+vantuylM@users.noreply.github.com>
2024-05-14 14:25:26 -0500

    Update 5/14/2024

---
+++ CSharp/StrInterview7/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrInterview7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string word = Console.ReadLine().ToLower();
+
+            int vowels = 0;
+            int cons = 0;
+
+            for (int lcv = 0; lcv < word.Length; lcv++)
+            {
+                char ltr = word[lcv];
+                if (ltr == 'a' || ltr == 'e' || ltr == 'i' || ltr == 'o' || ltr == 'u')
+                    vowels++;
+                else if (ltr >= 'a' && ltr <= 'z') cons++; // Check ASCII range
+            }
+            Console.WriteLine(word + " contains " + vowels + " vowels, and " + cons + "consonants");
+            Console.ReadLine();
+        }
+    }
+}
